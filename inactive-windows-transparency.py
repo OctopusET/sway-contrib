OctopusET/sway-contrib

commit 619783aa65f05fd1c430558a868f0c8695f9756f

Improve the robustness of inactive-windows-transparency
Only storing the window and workspace that were focused immediately
prior is not enough to handle all cases. For example, denoting by A1 the
1st window on workspace A, if we switch focus from A1 to B1 both A1 and
B1 will be opaque. If we then switch to A2, our prev_focused is B1 which
will remain opaque as its on a different workspace. The end result is
that A1, A2, and B1 are opaque, leaving two opaque windows on the same
workspace.

This commit switches to storing a set of all windows that are opaque. We
don't store their workspaces to attempt to be more robust if they are
moved. On a focus event, we walk this set, making windows that are on
the same workspace as the newly focused window translucent and removing
them from the set.

inactive-windows-transparency.py
@@ -14,24 +14,31 @@ import i3ipc
 
 
 def on_window_focus(args, ipc, event):
-    global prev_focused
-    global prev_workspace
+    global focused_set
 
-    focused_workspace = ipc.get_tree().find_focused()
+    # To get the workspace for a container, we need to have received its
+    # parents, so fetch the whole tree
+    tree = ipc.get_tree()
 
-    if focused_workspace is None:
-        return
+    focused_id = event.container.id
+    focused = tree.find_by_id(focused_id)
+    focused_workspace = focused.workspace()
 
-    focused = event.container
-    workspace = focused_workspace.workspace().num
+    focused.command("opacity " + args.focused)
+    focused_set.add(focused.id)
 
-    if focused.id != prev_focused.id:  # https://github.com/swaywm/sway/issues/2859
-        focused.command("opacity " + args.focused)
-        if workspace == prev_workspace:
-            prev_focused.command("opacity " + args.opacity)
-        prev_focused = focused
-        prev_workspace = workspace
+    to_remove = set()
+    for window_id in focused_set:
+        if window_id == focused.id:
+            continue
+        window = tree.find_by_id(window_id)
+        if window is None:
+            to_remove.add(window_id)
+        elif window.workspace() == focused_workspace:
+            window.command("opacity " + args.opacity)
+            to_remove.add(window_id)
 
+    focused_set -= to_remove
 
 def remove_opacity(ipc, focused_opacity):
     for workspace in ipc.get_tree().workspaces():
@@ -62,12 +69,12 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     ipc = i3ipc.Connection()
-    prev_focused = None
-    prev_workspace = ipc.get_tree().find_focused().workspace().num
+    focused_set = set()
 
     for window in ipc.get_tree():
         if window.focused:
-            prev_focused = window
+            focused_set.add(window.id)
+            window.command("opacity " + args.focused)
         else:
             window.command("opacity " + args.opacity)
     for sig in [signal.SIGINT, signal.SIGTERM]:

commit 0c4404d7acf4832c650abbf25fc2fa6377055b3a

Restore missing None check for focused window
Fixes: 619783a

inactive-windows-transparency.py
@@ -21,6 +21,9 @@ def on_window(args, ipc, event):
     tree = ipc.get_tree()
 
     focused = tree.find_focused()
+    if focused is None:
+        return
+
     focused_workspace = focused.workspace()
 
     focused.command("opacity " + args.focused)